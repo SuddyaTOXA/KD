ffa1ceb3776fbe30acbc067a02484d92a1b458bb
Anton <suddyadred@gmail.com>
2017-06-15 02:04:31 +0300

    suddya

--- assets/js/custom/scripts.js
+++ assets/js/custom/scripts.js
@@ -78,6 +78,13 @@ $(document).ready(function() {
         }
     });
 
+    //for smooth scroll
+        smoothScroll.init({
+            selector: '.smooth-scroll, a', // Selector for links (must be a class, ID, data attribute, or element tag)
+            speed: 500, // Integer. How fast to complete the scroll in milliseconds
+            easing: 'easeInQuad', // Easing pattern to use
+            offset: 130 // Integer. How far to offset the scrolling anchor location in pixels
+        });
 
     // for empty links
     $('.prevent').on('click', function(event){
